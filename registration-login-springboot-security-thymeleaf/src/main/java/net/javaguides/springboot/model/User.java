ecd9056cc584b1b138dbfe5db93a63611a8cb9be
Rutti <ruttib@gmail.com>
2022-06-08 00:22:44 -0400

    first commit

---
+++ registration-login-springboot-security-thymeleaf/src/main/java/net/javaguides/springboot/model/User.java
@@ -0,0 +1,96 @@
+package net.javaguides.springboot.model;
+
+import java.util.Collection;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	private String email;
+	private String password;
+	
+	// to fetch the roles while loading the user. if we want to load roles based on requirement use lazy
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL) 
+	@JoinTable(
+			name = "users_roles", 
+			joinColumns = @JoinColumn(columnDefinition = "user_id", referencedColumnName = "id"),
+			inverseJoinColumns = @JoinColumn(columnDefinition = "role_id", referencedColumnName = "id")
+			)
+	private Collection<Role> roles;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public User(String firstName, String lastName, String email, String password, Collection<Role> roles) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.roles = roles;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Collection<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(Collection<Role> roles) {
+		this.roles = roles;
+	}
+	
+	
+
+}
